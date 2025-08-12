36212b29de48e881071ffc77884bd37383efaf19
Raahim1400 <mraahimg@gmail.com>
2025-08-12 20:31:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import base64
 import pickle
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 # --- Page Config ---
